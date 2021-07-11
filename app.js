7361c236f9c4385e5cf9201779b95aee68b79ec8
kerry.olden@gmail.com <kerry.olden@gmail.com>
2021-07-10 22:35:51 -0500

    add app.js code

---
+++ app.js
@@ -0,0 +1,27 @@
+// grab references to the input element and the output div
+var text = d3.select("#text");
+var output = d3.select(".output");
+
+// Function to reverse a string
+function reverseString(str) {
+    return str.split("").reverse().join("");
+}
+
+// Function to handle input change
+function handleChange() {
+
+    console.log(d3.event)
+    // grab the value of the input field
+    var inputText = d3.event.target.value;
+
+    // clear the existing output
+    output.text("");
+
+    // reverse the input string
+    var reversedInput = reverseString(inputText);
+
+    // Set the output text to the reversed input string
+    output.text(reversedInput);
+}
+
+text.on("change", handleChange);
