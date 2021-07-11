44670eb71da2c50ebe6601c9047484386eed0aee
kerry.olden@gmail.com <kerry.olden@gmail.com>
2021-07-10 22:55:13 -0500

    more app.js work

--- app.js
+++ app.js
@@ -5,3 +5,12 @@ function buildMetadata(selection) {
     d3.json("samples.json").then((sampleData) => {
 
         console.log(sampleData);
+        
+    // Parse and filter the data to get the sample's metadata
+    var parsedData = sampleData.metadata;
+    console.log("parsed data inside buildMetadata function")
+    console.log(parsedData);
+
+    var sample = parsedData.filter(item => item.id == selection);
+    console.log("showing sample[0]:");
+    console.log(sample[0]);
